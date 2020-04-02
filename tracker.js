dd410d64d50df5699d242b6e855ab367073524dc
mathe1 <mathe@mathesoft.de>
2020-04-02 18:10:45 +0200

    Update class name for status label

--- tracker.js
+++ tracker.js
@@ -6,13 +6,13 @@ function get_time_diff () {
     var dif = currDate.getTime() - last_time.getTime();
     var TimeInSeconds = Math.abs(dif/1000);
 
-    var MINUTES = Math.floor(TimeInSeconds/60)
+    var MINUTES = Math.floor(TimeInSeconds/60);
     var SECONDS = Math.floor(TimeInSeconds%60);
     var FinalTime = 'Online for: ';
 
     if (MINUTES != 0)
         FinalTime = FinalTime + MINUTES + ' Minutes, ';
-    FinalTime += SECONDS + ' Seconds.'
+    FinalTime += SECONDS + ' Seconds.';
     console.log(FinalTime);
 }
 
@@ -20,15 +20,16 @@ setInterval(function() {
     var date = new Date();
 	var time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 	try {
-		var last_seen = document.getElementsByClassName("i1XSV")[0].innerText;
+		var last_seen = document.getElementsByClassName("O90ur")[0].innerText;		
 		if (checked === 0) {
 		    last_time = new Date(date);
 		    console.log(time + ' ' + last_seen);
 		    checked = 1;
 		}
+		if (checked >= 1) checked++;
 	}
 	catch(err) {
-		if (checked === 1) {
+		if (checked > 1) {
 			checked = 0;
 			console.log(time + ' offline');
 			get_time_diff();
